33c1f18acd12fece55133eaf8e732bceab7aebbf
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2024-03-09 12:35:09 -0300

    Update print.asm

---
+++ Graphics/print.asm
@@ -0,0 +1,48 @@
+[BITS    16]
+
+;SI = String (end with 0x00)
+;BL = Color
+printstr:
+        pusha
+
+        mov     ah, 0x0E
+        mov     al, [si]
+
+        printstr.loop:
+                int     0x10
+
+                inc     si
+                mov     al, [si]
+                
+                cmp     al, 0x00
+                jne     printstr.loop
+
+        popa
+
+        ret
+
+;AX = Debug string
+;SI = Message
+;BL = Debug color
+;BH = Message color
+debug:
+        push    bx
+        push    si
+
+        ;Print Debug Char
+        mov     si, ax
+        call    printstr
+
+        pop     si
+
+        ;Print Message
+        mov     bl, bh
+        call    printstr
+
+        pop     bx
+
+        ret
+
+DONE: db "[+] ", 0x00
+FAIL: db "[-] ", 0x00
+HOLD: db "[?] ", 0x00
