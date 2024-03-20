28329670c03626254b12e9cb5ac36a9958370e37
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2024-03-20 12:09:03 -0300

    Update print.asm
    Printing Decimals

--- Graphics/print.asm
+++ Graphics/print.asm
@@ -21,6 +21,53 @@ printstr:
 
         ret
 
+;AX = Value
+printdec:
+        pusha
+                xor cx, cx
+                xor dx, dx
+
+                printdec.loop:
+                        ;AX = AX / BX
+                        ;DX = REMAINDER
+                        cmp ax, 0x00
+                        je printdec.print
+
+                        mov bx, 0x0A
+                        div bx
+
+                        ;Save number
+                        push dx
+
+                        inc cx
+
+                        xor dx, dx
+                        jmp printdec.loop
+
+                printdec.print:
+                        cmp cx, 0x00
+                        je printdec.exit
+
+                        pop dx
+
+                        ;Converts to ASCII
+                        add dx, '0'
+
+                        ;Print number
+                        mov ah, 0x0E
+                        mov bl, 0x0F
+                        mov al, dl
+                        int 0x10
+
+                        dec cx
+
+                        jmp printdec.print
+
+printdec.exit:
+        popa
+
+        ret
+
 ;AX = Debug string
 ;SI = Message
 ;BL = Debug color
