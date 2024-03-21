06860194e87d12954089b5739a54a92113a96d64
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2024-03-21 12:32:19 -0300

    Update boot.asm

--- Bootloader/boot.asm
+++ Bootloader/boot.asm
@@ -1,7 +1,10 @@
 [BITS    16]
 [ORG 0x7C00]
 
-jmp short bootmain
+%define KERNELOFFSET 0x0000
+%define KERNELSEG    0x1000
+
+jmp short BootMain
 nop
 
 ;BIOS Parameter Block
@@ -10,7 +13,7 @@ BytesPerSectors:     dw 512
 SectorsPerClusters:  db 0x01
 ReservedSectors:     dw 0x01
 TotalFATs:           db 0x02
-RootDirEntries:      dw 224
+RootDirEntries:      dw 0xE0
 LogicalSectors:      dw 2880
 MediaDescriptorType: db 0xF0
 SectorsPerFAT:       dw 0x09
@@ -27,39 +30,32 @@ VolumeID:    dd 0x00
 VolumeLabel: db "ACIDOS     "
 FileSystem:  db "FAT12   "
 
-bootmain:
-        mov [DriveNumber], dl
+BootMain:
+        ;Save Drive's number
+        mov     [DriveNumber], dl
 
         ;Setup Data Segments
         xor     ax, ax
         mov     ds, ax
         mov     es, ax
 
-        ;Setup Stack (SP, SS)
+        ;Setup Stack
         mov     sp, 0x7C00
         mov     ss, ax
 
-        ;Set VESA 1920x1080
-        mov     ax, 0x4F02
-        mov     bx, 0x4117
-        int     0x10
+        call    ReadDrive
+        call    LoadRootDir
+        call    FindKernel
 
-        ;"[+] Booted from floppy..."
-        mov     si, bootmsg
-        mov     ax, DONE
-        mov     bl, 0x0A
-        mov     bh, 0x0F
-        call    debug
+        jmp     LoadKernel
 
-        jmp     $
-
-%include "Graphics/print.asm"
 %include "Bootloader/disk.asm"
 %include "Bootloader/fat.asm"
 
-bootmsg: db "Booted from floppy...", 0x0A, 0x0A, 0x0D, 0x00
-
-kernelfile: db "KERNEL  BIN"
+kernelbin:     db 'KERNEL  BIN'
+kernelcluster: dw 0x00
 
 times 510 - ($ - $$) db 0x00
 dw 0xAA55
+
+RootDirBuffer:
