72eb5dbb2fc1ce41f84035d98ec1a33a80f8801c
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2024-03-21 13:46:30 -0300

    Update kernel.asm

--- Kernel/kernel.asm
+++ Kernel/kernel.asm
@@ -1,9 +1,11 @@
 [BITS    16]
 [ORG 0x0000]
 
-call    SetupVESA
-
 KernelMain:
+        ;Set VGA Mode 13h
+        mov     ax, 0x13
+        int     0x10
+
         mov     si, welcomemsg
         mov     bl, 0x47
         call    printstr
@@ -13,6 +15,5 @@ KernelMain:
 welcomemsg: db "Welcome to AcidOS!", 0x0A, 0x0A, 0x0D, 0x00
 
 %include "Graphics/print.asm"
-%include "Graphics/vesa.asm"
 
 times (510 * 60) - ($ - $$) db 0x00
