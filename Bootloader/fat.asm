89aabb4c90a9aa3b211ffe01c8ebfade953ee9c4
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2024-03-21 12:33:34 -0300

    Update fat.asm

--- Bootloader/fat.asm
+++ Bootloader/fat.asm
@@ -40,17 +40,147 @@
 
 ;4 - Repeat until reaching and reading the file
 
-LoadRootDir:
-        ;RootDirSize (AX) = Entries * 32 (Entry size)
-        xor     dx, dx
-        mov     ax, 0x20
-        mul     word [RootDirEntries]
+;-------------------------------------------------------------
 
-        ;RootDirSectors (AX) = RootDirSize / BytesPerSectors
+LoadRootDir:
+        ;Clear for division and multiplication
         xor     dx, dx
+        xor     bx, bx
+
+        ;Compute the Root Directory Size
+        ;RootDirSize = (Entries * 32) / BytesPerSectors
+        mov     ax, [RootDirEntries]
+        shl     ax, 0x05
         div     word [BytesPerSectors]
 
-        ;CX = Root Dir Sectors
-        xchg    ax, cx
+        ;Compute the Root Directory's LBA
+        ;RootDirLBA = (TotalFATs * SectorsPerFAT) + ReservedSectors
+        mov     ax, [SectorsPerFAT]
+        mov     bl, [TotalFATs]
+        mul     bx
+        add     ax, [ReservedSectors]
+
+        ;Read the Root Directory
+        mov     cl, al
+        mov     dl, [DriveNumber]
+        mov     bx, RootDirBuffer
+        call    DiskRead
 
         ret
+
+;-------------------------------------------------------------
+
+FindKernel:
+        ;BX is a counter in the loop
+        ;DI is the RootDirBuffer to compare
+        xor     bx, bx
+        mov     di, RootDirBuffer
+
+        .searchKernel:
+                ;Compare up to 11 chars
+                mov     si, kernelbin
+                mov     cx, 11
+                push    di
+                repe    cmpsb
+                pop     di
+
+                je      .foundKernel
+                
+                ;Go to the next file
+                add     di, 0x20
+                inc     bx
+                
+                ;If it didn't found, then check again
+                cmp     bx, [RootDirEntries]
+                jl      .searchKernel
+
+                ;Everything was checked but didn't found
+                jmp     .failedKernel
+        
+        .foundKernel:
+                ;Gets kernel first logical cluster
+                mov     ax, [di + 26]
+                mov     [kernelcluster], ax
+
+                ;Load FAT to memory
+                mov     ax, [ReservedSectors]
+                mov     bx, RootDirBuffer
+                mov     cl, [SectorsPerFAT]
+                mov     dl, [DriveNumber]
+                call    DiskRead 
+
+        ret
+
+.failedKernel:
+        mov     ah, 0x0E
+        mov     al, 'X'
+        int     0x10
+
+        cli
+        hlt
+
+;-------------------------------------------------------------
+
+LoadKernel:
+        ;Read Kernel and process FAT chain
+        mov     bx, KERNELSEG
+        mov     es, bx
+        mov     bx, KERNELOFFSET
+
+        .kernelLoop:
+                ;Read next cluster
+                mov     ax, [kernelcluster]
+
+                mov     dl, [DriveNumber]
+                add     ax, 0x1F
+                mov     cl, 0x01
+                call    DiskRead
+
+                add     bx, [BytesPerSectors]
+
+                ;Get the location of the next cluster
+                mov     ax, [kernelcluster]
+                mov     cx, 0x03
+                mul     cx
+
+                mov     cx, 0x02
+                div     cx
+
+                ;AX = Index of entry in FAT
+                ;DX = Cluster % 2
+
+                ;Read entry from FAT table at index AX
+                mov     si, RootDirBuffer
+                add     si, ax
+                mov     ax, [ds:si]
+
+                or      dx, dx
+                jz      .even
+        
+        .odd:
+                shr     ax, 0x04
+                jmp     .nextCluster
+
+        .even:
+                and     ax, 0x0FFF
+
+        .nextCluster:
+                ;End of chain
+                cmp     ax, 0x0FF8
+                jae     .end
+
+                ;Loops again
+                mov     [kernelcluster], ax
+                jmp     .kernelLoop
+
+        .end:
+                mov     dl, [DriveNumber]
+
+                mov     ax, KERNELSEG
+                mov     ds, ax
+                mov     es, ax
+
+                jmp     KERNELSEG:KERNELOFFSET
+
+                cli
+                hlt
