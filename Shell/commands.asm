fda8e36a41415cee8f6fb3bf5c56904964bcc5ce
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2024-03-22 15:38:28 -0300

    Update commands.asm

--- Shell/commands.asm
+++ Shell/commands.asm
@@ -1,1 +1,50 @@
+[BITS    16]
 
+RunCommand:
+        pusha
+
+        ;"clear" command
+        mov     si, cmdbuffer
+        mov     di, clear
+        mov     cx, 0x06
+        repe    cmpsb
+        jcxz    .exeClear
+
+        ;"waitkey" command
+        mov     si, cmdbuffer
+        mov     di, waitkey
+        mov     cx, 0x08
+        repe    cmpsb
+        jcxz    .exeWaitKey
+
+        ;In case of unknown commands
+        mov     word [cursorX], 0x00
+        inc     word [cursorY]
+
+        mov     si, unknowncmd
+        mov     al, 0x0C
+        call    PrintString
+
+        popa
+
+        ret
+
+.exeClear:
+        mov     word [cursorX], 0x00
+        mov     word [cursorY], 0x00
+
+        call    ClearScreen
+
+        popa
+
+        ret
+    
+.exeWaitKey:
+        call    ReadKey
+
+        popa
+
+        ret
+
+clear: db "clear"
+waitkey: db "waitkey"
