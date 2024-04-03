df5ee9cb93039a612c3973cf5dad879aa204f6b3
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2024-04-03 20:20:01 -0300

    Update mem.asm

--- Memory/mem.asm
+++ Memory/mem.asm
@@ -1,1 +1,35 @@
+[BITS    16]
 
+DetectXMS:
+        pusha
+        
+        clc
+
+        ;Get XMS information
+        mov     eax, 0xE801
+        int     0x15
+
+        jc      xmsfailed
+
+        mov     si, XMSOK
+        mov     al, 74
+        call    PrintString
+
+        mov     ax, bx
+        call    PrintDec
+
+        popa
+
+        ret
+
+xmsfailed:
+        mov     si, XMSFAIL
+        mov     al, 40
+        call    PrintString
+
+        popa
+
+        ret
+
+XMSOK: db "Total extended memory (kb): ", 0x00
+XMSFAIL: db "Failed to detect extended memory!", 0x00
