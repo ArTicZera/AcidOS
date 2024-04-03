c560bb8599cc887ec45b9684211af3ad8b22291d
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2024-04-03 20:18:42 -0300

    Update shell.asm

--- Shell/shell.asm
+++ Shell/shell.asm
@@ -3,7 +3,7 @@
 %define PROMPTCOLOR 0x47
 %define TEXTCOLOR   0x0F
 
-%define BUFFSIZE 0x20
+%define BUFFSIZE 0x40
 
 Shell:
         call    PrintLocalDir
@@ -31,6 +31,7 @@ GetCommand:
         call    ReadKey
 
         xor     si, si
+        mov     bl, 0x0F
 
         .cmdLoop:
                 mov     [cmdbuffer + si], al
@@ -39,21 +40,7 @@ GetCommand:
                 je      .backspace
 
         .printLetter:
-                push    si
-
-                xor     bx, bx
-                mov     bl, al
-                shl     bx, 0x03
-
-                mov     si, ProggyFont
-                add     si, bx
-
-                mov     al, TEXTCOLOR
-
-                call    DrawChar
-
-                pop     si
-
+                call    PrintChar
                 call    ReadKey
 
                 jmp     .compare
@@ -68,6 +55,8 @@ GetCommand:
                 jmp     .printLetter
 
         .compare:
+                cmp     al, 0x20
+                je      getarg
 
                 inc     si
 
@@ -78,11 +67,55 @@ GetCommand:
 
         ret
 
+getarg:
+        call    PrintChar
+        call    ReadKey
+        
+        xor     si, si
+        mov     bl, 0x0F
+
+        .argLoop:
+                mov     [argbuffer + si], al
+
+                cmp     al, 0x08
+                je      .argBackspace
+
+        .argPrintLetter:
+                call    PrintChar
+                call    ReadKey
+
+                jmp     .argCompare
+
+        .argBackspace:
+                dec     si
+
+                mov     [argbuffer + si], al
+
+                dec     word [cursorX]
+
+                jmp     .argPrintLetter
+        
+        .argCompare:
+                inc     si
+
+                cmp     al, 0x0D
+                jne     .argLoop
+        popa
+
+        ret
+
 ResetBuffer:    
         pusha
 
+        ;Reset cmd buffer
         mov     di, cmdbuffer
         mov     cx, BUFFSIZE
+        mov     al, 0x00
+        rep     stosb
+
+        ;Reset arg buffer
+        mov     di, argbuffer
+        mov     cx, BUFFSIZE/2
         mov     al, 0x00
         rep     stosb
 
@@ -93,8 +126,7 @@ ResetBuffer:
 PrintEndLine:
         pusha
 
-        mov     word [cursorX], 0x00
-        add     word [cursorY], 0x01
+        call    NextLine
 
         popa
 
@@ -104,5 +136,6 @@ endl: db 0x0A, 0x0A, 0x0D
 localdir: db ">/", 0x00
 
 cmdbuffer: times BUFFSIZE db 0x00
+argbuffer: times BUFFSIZE / 2 db 0x00
 
 unknowncmd: db "Unknown command, please try again!", 0x00 
