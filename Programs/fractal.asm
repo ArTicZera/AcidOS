a6a2b8891a8f0df2f8f6ab7724ced731f21c8139
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2024-04-07 08:44:34 -0300

    Update fractal.asm

--- Programs/fractal.asm
+++ Programs/fractal.asm
@@ -7,14 +7,17 @@
 %define HSCREEN 200
 
 ProgramMain:
+        ;Set Pixel Function
         mov     ah, 0x0C
 
+        ;X = 0, Y = 0
         xor     cx, cx
         xor     dx, dx
 
         .palette:
                 push    cx
 
+                ;AL = (X ^ Y) / 2
                 xor     cx, dx
                 mov     al, cl
                 shr     al, 0x01
@@ -30,20 +33,24 @@ ProgramMain:
 
                 int     0x10
 
+                ;Next pixel
                 inc     cx
                 
                 jmp     .palette
 
         .nextLine:
+                ;X = 0, Y++
                 xor     cx, cx
                 inc     dx
 
                 jmp     .palette
 
 .end:
+        ;Wait for key
         xor     ax, ax
         int     0x16
 
+        ;Clear Screen
         mov     ax, 0x13
         int     0x10
 
