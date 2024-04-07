8b5c9790ff027e39583e841cb13b27f71bcd58a1
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2024-04-07 08:46:40 -0300

    Update stars.asm

--- Programs/stars.asm
+++ Programs/stars.asm
@@ -24,32 +24,38 @@ ProgramMain:
         push    bx
         push    dx
 
+        ;Linear congruential generator
         mov     ax, [seed]
         add     ax, [seed]
         add     ax, [seed]
         mov     [seed], ax
 
+        ;Index = seed % WSCREEN
         xor     dx, dx
         mov     bx, WSCREEN
         div     bx
 
+        ;X = Random position
         mov     cx, dx
 
         pop     dx
         pop     bx
         pop     ax
 
+        ;Set Pixel
         int     0x10
 
         ;Next Index
         inc     bx
 
+        ;Next line
         inc     dx
 
         ;Checks if it reached the max index
         cmp     bx, STARS
         jb      .starsLoop
 
+        ;Wait key
         xor     ax, ax
         int     0x16
 
