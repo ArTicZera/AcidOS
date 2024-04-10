068a368d96e5c1ab4fd51c24e135ea43686362cb
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2024-04-10 11:58:38 -0300

    Update make.sh

--- make.sh
+++ make.sh
@@ -14,6 +14,7 @@ nasm -fbin Programs/fractal.asm -o $BIN/fractal.com
 nasm -fbin Programs/stars.asm   -o $BIN/stars.com
 nasm -fbin Programs/plasma.asm  -o $BIN/plasma.com
 nasm -fbin Programs/fibo.asm    -o $BIN/fibo.com
+nasm -fbin Programs/cube.asm    -o $BIN/cube.com
 
 echo -e "\e[33;40mMounting Image File!\e[0m\n"
 dd if=/dev/zero of=AcidOS.img bs=512 count=2880
@@ -24,6 +25,7 @@ mcopy -i AcidOS.img $BIN/fractal.com "::fractal.com"
 mcopy -i AcidOS.img $BIN/stars.com "::stars.com"
 mcopy -i AcidOS.img $BIN/plasma.com "::plasma.com"
 mcopy -i AcidOS.img $BIN/fibo.com "::fibo.com"
+mcopy -i AcidOS.img $BIN/cube.com "::cube.com"
 
 if [ $? -eq 0 ]; then
     echo -e "\n\e[36;40mCompiled successfully!\e[0m\n"
