ff1ad26bbe54250371ab1f4e4990e5de004e60f7
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2024-04-11 17:50:06 -0300

    Update make.sh

--- make.sh
+++ make.sh
@@ -10,7 +10,6 @@ nasm -fbin Bootloader/boot.asm -o $BIN/boot.bin
 nasm -fbin Kernel/kernel.asm -o $BIN/kernel.bin
 
 echo -e "\n\e[33;40mCompiling programs!\e[0m\n"
-nasm -fbin Programs/fractal.asm -o $BIN/fractal.com
 nasm -fbin Programs/stars.asm   -o $BIN/stars.com
 nasm -fbin Programs/plasma.asm  -o $BIN/plasma.com
 nasm -fbin Programs/fibo.asm    -o $BIN/fibo.com
@@ -21,11 +20,11 @@ dd if=/dev/zero of=AcidOS.img bs=512 count=2880
 mkfs.fat -F 12 -n "ACIDOS" AcidOS.img
 dd if=$BIN/boot.bin of=AcidOS.img conv=notrunc
 mcopy -i AcidOS.img $BIN/kernel.bin "::kernel.bin"
-mcopy -i AcidOS.img $BIN/fractal.com "::fractal.com"
 mcopy -i AcidOS.img $BIN/stars.com "::stars.com"
 mcopy -i AcidOS.img $BIN/plasma.com "::plasma.com"
 mcopy -i AcidOS.img $BIN/fibo.com "::fibo.com"
 mcopy -i AcidOS.img $BIN/cube.com "::cube.com"
+mcopy -i AcidOS.img $BIN/cube.com "::doom.com"
 
 if [ $? -eq 0 ]; then
     echo -e "\n\e[36;40mCompiled successfully!\e[0m\n"
