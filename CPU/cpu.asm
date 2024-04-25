c1951fbd69d2d11ec9a6deb74504c214b08b0868
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2024-04-25 16:10:01 -0300

    Update cpu.asm

--- CPU/cpu.asm
+++ CPU/cpu.asm
@@ -1,1 +1,18 @@
+[BITS    16]
 
+GetCPUInfo:
+        pusha
+
+        xor     eax, eax
+        cpuid
+        mov     [vendor + 8], ebx
+        mov     [vendor + 12], edx
+        mov     [vendor + 16], ecx
+
+        popa
+
+        ret
+
+vendor:
+        db "Vendor: "
+        times 13 db 0x00
